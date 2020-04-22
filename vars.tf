da82169e583f45e1315f246043d7bce7d51f1f7a
satyadhenuvakonda <satya.dhenuvakonda@gmail.com>
2020-04-22 21:04:14 +0100

    adding

---
+++ vars.tf
@@ -0,0 +1,30 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-047bb4163c506cd98"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "k8s-key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "k8s-key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
+
+
